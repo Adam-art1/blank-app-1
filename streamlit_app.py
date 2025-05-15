df04da3c10f2e99d0d3edbc9ab99e8fd4c7c9d81
Adam-art1 <blank7921@gmail.com>
2025-05-15 13:52:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
 st.title("Punya gweh 😋")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-st.image("view/young.png", width=200) 
+st.write("DINGIN TETAPI TIDAK KEJAM 🥶") 
 st.image("./17472912470587405037618347906961.jpg", width=200)
