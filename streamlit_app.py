5e63aaa92c8a4e76c2e67b3f2ecb9878033d493b
Adam-art1 <blank7921@gmail.com>
2025-05-15 14:27:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
 st.title("Punya gweh 😋")
-st.write("DINGIN TETAPI TIDAK KEJAM 🥶") 
 st.image("./17472912470587405037618347906961.jpg", width=200)
+st.write("DINGIN TETAPI TIDAK KEJAM 🥶")
+st.subheader("Adam nugget")
+st.write("PENTOLLLLL") 
