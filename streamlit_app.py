6540c12da4562021024f475733facae3ad3e7de4
Adam-art1 <blank7921@gmail.com>
2025-05-15 13:47:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("./17472912470587405037618347906961.jpg")
